4be4b6628f45821dc92e44c58f72e98db74468cc
Anjali-345 <anjaligarikapati127@gmail.com>
2025-03-14 12:42:41 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import google.generativeai as genai
-api_key = "AIzaSyCA6b-YoCXC543TPdMm7OnNF6sayTktKbA"  # Replace with your actual API key
+api_key = "AIzaSyAmL9fw0we10uFP9RF9Hexk6hRwLVpDS6s"  # Replace with your actual API key
 genai.configure(api_key=api_key)
 
 # Define generation settings
